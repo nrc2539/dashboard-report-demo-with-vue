c8e8edd69accd8c61130b03ff739672ad8154933
nrc2539 <narongchai.b2539@gmail.com>
2020-11-10 14:56:15 +0700

    add file for github page

---
+++ docs/precache-manifest.8f76006bb3d8eb05aedac60e865d7e39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70af01eb96555395817",
+    "url": "/dashboard-demo-vue-app/css/about.feed073f.css"
+  },
+  {
+    "revision": "1b8e7bc92051c41548d3",
+    "url": "/dashboard-demo-vue-app/css/app.603cdca3.css"
+  },
+  {
+    "revision": "f02aaa24359e2534baf0",
+    "url": "/dashboard-demo-vue-app/css/chunk-vendors.93223636.css"
+  },
+  {
+    "revision": "b515686f26620c2eb78d7eacefc5e6db",
+    "url": "/dashboard-demo-vue-app/index.html"
+  },
+  {
+    "revision": "b70af01eb96555395817",
+    "url": "/dashboard-demo-vue-app/js/about.63c246ac.js"
+  },
+  {
+    "revision": "1b8e7bc92051c41548d3",
+    "url": "/dashboard-demo-vue-app/js/app.dca2c60b.js"
+  },
+  {
+    "revision": "f02aaa24359e2534baf0",
+    "url": "/dashboard-demo-vue-app/js/chunk-vendors.5a35ff9c.js"
+  },
+  {
+    "revision": "65d5d7aeeb7d7cb9a089e18d2429e260",
+    "url": "/dashboard-demo-vue-app/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/dashboard-demo-vue-app/robots.txt"
+  }
+]);
